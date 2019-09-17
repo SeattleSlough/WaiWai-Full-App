e00723c6921e28f5e958f232ed72e2cd761faed0
Michael J Maggs <michaelmaggs@gmail.com>
2019-09-17 15:29:47 -0700

    filter working

--- waiwai_backend/waiwai_backend_2/app/controllers/activities_controller.rb
+++ waiwai_backend/waiwai_backend_2/app/controllers/activities_controller.rb
@@ -8,6 +8,12 @@ end
 def create 
     @actvitiy = Activity.create(name: params[:name], provider: params[:provider], description: params[:description], image: params[:image], cost: params[:cost])
 end
+
+def filter
+    @activities = Activity.all 
+    @filter = @activities.slice(request.headers['HTTP_INDEX'].to_i,5)
+    render json: @filter
+end
     
     
 
