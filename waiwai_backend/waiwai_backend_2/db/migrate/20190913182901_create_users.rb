a54b0ad4cd4c30aece84cbf736b0deff5f80c85f
Michael J Maggs <michaelmaggs@gmail.com>
2019-09-19 11:21:01 -0700

    user foreign keys added

--- waiwai_backend/waiwai_backend_2/db/migrate/20190913182901_create_users.rb
+++ waiwai_backend/waiwai_backend_2/db/migrate/20190913182901_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+      t.integer :hotel_id
+      t.integer :car_id
 
       t.timestamps
     end
--- waiwai_backend/waiwai_backend_2/db/schema.rb
+++ waiwai_backend/waiwai_backend_2/db/schema.rb
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2019_09_14_041624) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "hotel_id"
+    t.integer "car_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
