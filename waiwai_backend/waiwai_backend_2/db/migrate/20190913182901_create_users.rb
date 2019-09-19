bd7a0149dee39bb653a7eddd66690076ebf58f5f
Michael J Maggs <michaelmaggs@gmail.com>
2019-09-19 12:54:34 -0700

    associations set up (sans car), auth working

--- waiwai_backend/waiwai_backend_2/db/migrate/20190913182901_create_users.rb
+++ waiwai_backend/waiwai_backend_2/db/migrate/20190913182901_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.integer :hotel_id
-      t.integer :car_id
 
       t.timestamps
     end
